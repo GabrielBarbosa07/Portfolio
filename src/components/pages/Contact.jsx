ad11e3082449402507d92a7a6677a156a11d57d2
Gabriel Barbosa <gabrielbarbosadorio@gmail.com>
2022-09-25 21:25:33 -0300

    Implementação do envio de email

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Heading } from '../common/Heading'
 import { contato } from '../data/gbdata'
+import emailjs from "@emailjs/browser"
 
 export const Contact = () => {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [suggestion, setSuggestion] = useState("")
+    const [message, setMessage] = useState("")
+
+    function sendEmail(e) {
+        e.preventDefault()
+
+        if (name === "" || email === "" || message === "") {
+            alert(`Preencha todos os campos`)
+            return
+        }
+
+        const templateParams = {
+            from_name: name,
+            suggestion,
+            message,
+            email
+        }
+
+        emailjs.send("service_kkipa27", "template_7lyzqet", templateParams, "myuSVVgifW2GlXR4c")
+            .then((res) => {
+                setName("")
+                setEmail("")
+                setSuggestion("")
+                setMessage("")
+            }, (err) => {
+                console.log("ERRO:", err);
+            })
+
+            alert("Email Enviado!")
+    }
+
     return (
         <>
             <div className="contact">
@@ -12,13 +46,35 @@ export const Contact = () => {
                     <div className="content flexsb">
 
                         <div className="right">
-                            <form >
+                            <form onSubmit={sendEmail}>
                                 <div className="flex">
-                                    <input type="text" placeholder='Nome' data-aos="zoom-in-down" />
-                                    <input type="email" placeholder='Email' data-aos="zoom-in-up" />
+                                    <input
+                                        type="text"
+                                        placeholder='Nome'
+                                        data-aos="zoom-in-down"
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)} />
+                                    <input
+                                        type="email"
+                                        placeholder='Email'
+                                        data-aos="zoom-in-up"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)} />
                                 </div>
-                                <input type="text" placeholder='Sugestão' data-aos="zoom-in-up" />
-                                <textarea data-aos="zoom-in-down" cols="30" rows="10"></textarea>
+                                <input
+                                    type="text"
+                                    placeholder='Sugestão'
+                                    data-aos="zoom-in-up"
+                                    value={suggestion}
+                                    onChange={(e) => setSuggestion(e.target.value)}
+                                />
+                                <textarea
+                                    data-aos="zoom-in-down"
+                                    cols="30"
+                                    rows="10"
+                                    placeholder='Mensagem'
+                                    value={message}
+                                    onChange={(e) => setMessage(e.target.value)}></textarea>
                                 <button className='buttonDefault' data-aos="zoom-in-down">Enviar</button>
                             </form>
                         </div>
